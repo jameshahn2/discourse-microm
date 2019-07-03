d53fb4042057c3d7ac960d95e7fb4d65dea3d8d0
James Hahn II <james@triberocket.com>
2019-07-02 20:40:34 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,45 +5,11 @@
 # url: https://github.com/jameshahn2
 
 
-register_asset "stylesheets/discourse-microm.scss"
+enabled_site_setting :composer_audio_upload_enabled
 
+register_asset 'stylesheets/discourse-microm.scss'
 
-enabled_site_setting :discourse_microm_enabled
-
-PLUGIN_NAME ||= "DiscourseMicrom".freeze
-
-after_initialize do
-  
-  # see lib/plugin/instance.rb for the methods available in this context
-  
-
-  module ::DiscourseMicrom
-    class Engine < ::Rails::Engine
-      engine_name PLUGIN_NAME
-      isolate_namespace DiscourseMicrom
-    end
-  end
-
-  
-
-  
-  require_dependency "application_controller"
-  class DiscourseMicrom::ActionsController < ::ApplicationController
-    requires_plugin PLUGIN_NAME
-
-    before_action :ensure_logged_in
-
-    def list
-      render json: success_json
-    end
-  end
-
-  DiscourseMicrom::Engine.routes.draw do
-    get "/list" => "actions#list"
-  end
-
-  Discourse::Application.routes.append do
-    mount ::DiscourseMicrom::Engine, at: "/discourse-microm"
-  end
-  
+register_html_builder('server:before-head-close') do
+  #'<script src="/plugins/discourse-audio-upload/javascripts/vendor/RecordRTC.min.js"></script>'
+  '<script src="/plugins/discourse-audio-upload/javascripts/vendor/microm.min.js"></script>'
 end
