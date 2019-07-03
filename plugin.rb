453c094dbfc948814c0a15e216c90a7555f57a52
James Hahn II <james@triberocket.com>
2019-07-02 19:59:09 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,6 @@ enabled_site_setting :composer_audio_upload_enabled
 register_asset 'stylesheets/audio-upload.scss'
 
 register_html_builder('server:before-head-close') do
-  #'<script src="/plugins/discourse-audio-upload/javascripts/vendor/RecordRTC.min.js"></script>'
-  '<script src="/plugins/discourse-audio-upload/javascripts/vendor/microm.min.js"></script>'
+  #'<script src="/plugins/discourse-microm/javascripts/vendor/RecordRTC.min.js"></script>'
+  '<script src="/plugins/discourse-microm/javascripts/vendor/microm.min.js"></script>'
 end
